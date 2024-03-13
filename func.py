b289e717fc7d3a2dd0bdc95ebcc124a98d774738
blacklightsun <blacklightsun@gmail.com>
2024-03-13 15:27:18 +0200

    new command set commit

---
+++ func.py
@@ -0,0 +1,223 @@
+
+from contextlib import contextmanager
+from classes import NameFormatError, PhoneFormatError, BirthdayFormatError, EmailFormatError, Field, Name, Phone, Birthday, Record, AddressBook
+
+@contextmanager
+def catch_my_exceptions(*exceptions):
+    try:
+        yield
+    except NameFormatError:
+        print("Wrong name")
+    except PhoneFormatError:
+        print("Wrong phone")
+    except BirthdayFormatError:
+        print("Wrong b-day")
+    except EmailFormatError:
+        print("Wrong email")
+
+def input_error(func):
+    '''
+    Судячи з усього - це декоратор обробки помилок
+    '''
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            return "Give me name and phone please."
+        except IndexError:
+            return "Give me name please."
+        except KeyError:
+            return "No such contact."
+        except NameFormatError:
+            return "Wrong name format."
+        except PhoneFormatError:
+            return "Wrong phone format."
+        except BirthdayFormatError:
+            return "Wrong birthday format."
+        except EmailFormatError:
+            return "Wrong email format."
+    return inner
+
+def parse_input(user_input: str):
+    '''
+    Парсер введеного рядка
+    '''
+    cmd_string, *args = user_input.split()
+    cmd_string = cmd_string.strip().lower()
+    argums_tuple = (*args,)
+    return cmd_string, argums_tuple
+
+def help():
+    '''
+    Друк сторінки з синтаксисом команд
+    '''
+    print("Usage:")    #################### наповнити змістом #######################
+
+def hello():
+    '''
+    Друк сторінки з привітанням
+    '''
+    print("How can I help you?") #################### наповнити змістом #######################
+
+@input_error
+def add_man(argums):
+    '''
+    Функція додавання контакту
+    '''
+    pass #################### наповнити кодом #######################
+
+@input_error
+def del_man(argums):
+    '''
+    Функція видалення контакту
+    '''
+    pass #################### наповнити кодом #######################
+
+@input_error
+def cng_man(argums):
+    '''
+    Функція зміни/оновлення контакту
+    '''
+    pass #################### наповнити кодом #######################
+
+@input_error
+def show_man(argums):
+    '''
+    Функція показу даних контакту
+    '''
+    pass #################### наповнити кодом #######################
+
+@input_error
+def find_man(argums):
+    '''
+    Функція пошуку контакту
+    '''
+    pass #################### наповнити кодом #######################
+
+@input_error
+def add_phone(argums):
+    '''
+    Функція додавання номеру(-ів) телефону(-ів)
+    '''
+    pass #################### наповнити кодом #######################
+
+@input_error
+def cng_phone(argums):
+    '''
+    Функція зміни номеру телефону
+    '''
+    pass  #################### наповнити кодом #######################
+
+@input_error
+def del_phone(argums):
+    '''
+    Функція зміни номеру телефону
+    '''
+    pass  #################### наповнити кодом #######################
+
+@input_error
+def add_email(argums):
+    '''
+    Функція додавання email
+    '''
+    pass  #################### наповнити кодом #######################
+
+@input_error
+def cng_email(argums):
+    '''
+    Функція додавання email
+    '''
+    pass  #################### наповнити кодом #######################
+
+@input_error
+def find_email(argums):
+    '''
+    Функція пошуку email
+    '''
+    pass  #################### наповнити кодом #######################
+
+@input_error
+def del_email(argums):
+    '''
+    Функція видаленя email
+    '''
+    pass  #################### наповнити кодом #######################
+
+@input_error
+def add_bday(argums):
+    '''
+    Функція додавання дня народження
+    '''
+    pass  #################### наповнити кодом #######################
+
+@input_error
+def cng_bday(argums):
+    '''
+    Функція додавання дня народження
+    '''
+    pass  #################### наповнити кодом #######################
+
+@input_error
+def del_bday(argums):
+    '''
+    Функція видалення дня народження
+    '''
+    pass  #################### наповнити кодом #######################
+
+@input_error
+def show_bday(argums):
+    '''
+    Функція видалення дня народження
+    '''
+    pass  #################### наповнити кодом #######################
+
+@input_error
+def add_adr(argums):
+    '''
+    Функція додавання адреси
+    '''
+    pass  #################### наповнити кодом #######################
+
+@input_error
+def del_adr(argums):
+    '''
+    Функція видалення адреси
+    '''
+    pass  #################### наповнити кодом #######################
+
+@input_error
+def find_adr(argums):
+    '''
+    Функція пошуку адреси
+    '''
+    pass  #################### наповнити кодом #######################
+
+
+@input_error
+def add_note(argums):
+    '''
+    Функція додавання нотатки
+    '''
+    pass  #################### наповнити кодом #######################
+
+@input_error
+def del_note(argums):
+    '''
+    Функція додавання нотатки
+    '''
+    pass  #################### наповнити кодом #######################
+
+@input_error
+def find_note(argums):
+    '''
+    Функція додавання нотатки
+    '''
+    pass  #################### наповнити кодом #######################
+
+@input_error
+def show_note(argums):
+    '''
+    Функція додавання нотатки
+    '''
+    pass  #################### наповнити кодом #######################
+
--- main.py
+++ main.py
@@ -1,139 +1,110 @@
-"""
-    AddressBook Bot
-"""
-import AddressBook as AB
+from classes import AddressBook, NoteBook # модуль з класами
+import func # модуль з функціями базового функціоналу
+import features # модуль з фічами
 
-def input_error(func):
-    def inner(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except ValueError:
-            return "Give me name and phone please."
-        except IndexError:
-            return "Give me name please."
-        except KeyError:
-            return "No such contact."
-        except AB.NameFormatError:
-            return "Wrong name format."
-        except AB.PhoneFormatError:
-            return "Wrong phone format."
-        except AB.BirthdayFormatError:
-            return "Wrong birthday format."
-        except AB.EmailFormatError:
-            return "Wrong email format."
-    return inner
-
-def parse_input(user_input):
-    cmd, *args = user_input.split()
-    cmd = cmd.strip().lower()
-    return cmd, *args
-
-def print_help(): # FIXME: Wrong commands
-    return "Available commands:\n" + \
-           "\tadd [name] [phone]\n" + \
-           "\tchange [name] [old_phone] [new_phone]\n" + \
-           "\tphone [name]\n" + \
-           "\tadd-birthday [name] [birthday]\n" + \
-           "\tshow-birthday [name]\n" + \
-           "\tbirthdays\n" + \
-           "\tall\n" + \
-           "\texit"
-
-@input_error
-def add_contact(args, book):
-    if len(args) == 2:
-        name, phone_num = args
-        record = book.find(name)
-        if not record:
-            record = AB.Record(name)
-            book.add_record(record)
-        record.add_phone(phone_num)
-        book.save()
-        return "Contact added."
-    return "[ERROR] Expected command: add [name] [phone]"
-
-@input_error
-def change_contact(args, book):
-    if len(args) == 3:
-        name, old_phone, new_phone = args
-        record = book.find(name)
-        if record:
-            record.edit_phone(old_phone, new_phone)
-            book.save()
-            return "Contact updated."
-        return "[ERROR] No such contact."
-    return "[ERROR] Expected command: change [name] [phone]"
-
-@input_error
-def phone(args, book):
-    if len(args) == 1:
-        name = args[0]
-        record = book.find(name)
-        if record:
-            return record
-        return "[ERROR] No such contact."
-    return "[ERROR] Expected command: phone [name]"
-
-@input_error
-def add_birthday(args, book):
-    if len(args) == 2:
-        name, birthday = args
-        record = book.find(name)
-        if record:
-            record.add_birthday(birthday)
-            book.save()
-            return "Birthday added."
-        return "[ERROR] No such contact."
-    return "[ERROR] Expected command: add-birthday [name] [birthday]"
-
-@input_error
-def show_birthday(args, book):
-    if len(args) == 1:
-        name = args[0]
-        record = book.find(name)
-        if record:
-            return record.birthday
-        return "[ERROR] No such contact."
-    return "[ERROR] Expected command: show-birthday [name]"
-
-@input_error
-def birthdays(book):
-    book.get_birthdays_per_week()
 
 def main():
-    book = AB.AddressBook()
-    book.load()
-    print("Welcome to the assistant bot!")
+    '''
+     Main function
+    '''
+
+    # ініціалізація
+    book = AddressBook.load()
+    notes = NoteBook.load()
+    features.greeting()
+
+    # work cycle
     while True:
-        user_input = input("Enter a command: ")
+
+        # parsing
+        user_input = input("Enter a command >>> ")
         if user_input:
-            command, *args = parse_input(user_input)
+            command, argums = func.parse_input(user_input)
         else:
             continue
-        if command in ["close", "exit", "quit"]:
-            print("Good bye!")
-            book.save()
-            break
-        if command == "hello":
-            print("How can I help you?")
-        elif command == "help":
-            print(print_help())
-        elif command == "add":
-            print(add_contact(args, book))
-        elif command == "change":
-            print(change_contact(args, book))
-        elif command == "phone":
-            print(phone(args, book))
-        elif command == "add-birthday":
-            print(add_birthday(args, book))
-        elif command == "show-birthday":
-            print(show_birthday(args,book))
-        elif command == "birthdays":
-            birthdays(book)
-        elif command == "all":
-            print(book)
-        else:
-            print("Invalid command.")
 
+        # handling
+        if command in ("close", "exit", "quit"):
+            book.save()
+            features.goodbye()
+            break
+
+        elif command == "help":
+            func.help()
+
+        elif command == "hello":
+            func.hello()
+
+        elif command == "add-man":
+            func.add_man(argums)
+
+        elif command == "del-man":
+            func.del_man(argums)
+
+        elif command == "cng-man":
+            func.cng_man(argums)
+
+        elif command == "show-man":
+            func.show_man(argums)
+
+        elif command == "find-man":
+            func.find_man(argums)
+
+        elif command == "add-phone":
+            func.add_phone(argums)
+
+        elif command == "cng-phone":
+            func.cng_phone(argums)
+
+        elif command == "del-phone":
+            func.del_phone(argums)
+
+        elif command == "add-email":
+            func.add_email(argums)
+
+        elif command == "cng-email":
+            func.cng_email(argums)
+
+        elif command == "find-email":
+            func.find_email(argums)
+
+        elif command == "del-email":
+            func.del_email(argums)
+
+        elif command == "add-bday":
+            func.add_bday(argums)
+
+        elif command == "cng-bday":
+            func.cng_bday(argums)
+
+        elif command == "del-bday":
+            func.del_bday(argums)
+
+        elif command == "show-bday":
+            func.show_bday(argums)
+
+        elif command == "add-adr":
+            func.add_adr(argums)
+
+        elif command == "del-adr":
+            func.del_adr(argums)
+
+        elif command == "find-adr":
+            func.find_adr(argums)
+
+        elif command == "add-note":
+            func.add_note(argums)
+
+        elif command == "del-note":
+            func.del_note(argums)
+
+        elif command == "find-note":
+            func.find_note(argums)
+
+        elif command == "show-note":
+            func.show_note(argums)
+
+        else:
+            print("Invalid command. Type 'help' for get a list of commands.")
 if __name__ == "__main__":
     main()
