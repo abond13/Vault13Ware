4c538f846f4c7248df94ed7d168fd5ce0c12a30e
Danylo <virus1908@gmail.com>
2024-03-14 19:48:10 +0200

    Added initial notes feature Fixed formatting

--- classes.py
+++ classes.py
@@ -20,7 +20,20 @@ class BirthdayFormatError(Exception):
 class EmailFormatError(Exception):
     pass
 
+
 class AddressFormatError(Exception):
+    pass
+
+
+class NoTextError(Exception):
+    pass
+
+
+class NoIdEnteredError(Exception):
+    pass
+
+
+class NoIdFoundError(Exception):
     pass
 
 
@@ -28,11 +41,13 @@ class Field:
     """
     Базовий клас для полів запису.
     """
+
     def __init__(self, value):
         self.value = value
 
     def __str__(self):
         return str(self.value)
+
 
 class Name(Field):
     """
@@ -42,10 +57,12 @@ class Name(Field):
             2. Перша літера велика.
                 Наприклад: Al
     """
+
     def __init__(self, name):
         if not re.fullmatch("[A-Z][a-z]+", name):
             raise NameFormatError
         super().__init__(name)
+
 
 class Phone(Field):
     """
@@ -54,28 +71,34 @@ class Phone(Field):
         1. Складається з цифр.
         2. Довжина повинна бути 10 символів.
     """
+
     def __init__(self, phone):
         if not re.fullmatch("[0-9]{10}", phone):
             raise PhoneFormatError
         super().__init__(phone)
 
-class Birthday(datetime.date): # Why not Field? #######################
+
+class Birthday(datetime.date):  # Why not Field? #######################
     """
     Клас для зберігання дня народження.
     Формат виводу: DD.MM.YYYY
     """
+
     def __str__(self):
         return f"{self.day:02}.{self.month:02}.{self.year:04}"
+
 
 class Email(Field):
     """
     Клас для зберігання email.
     Email має задоволняти формату login@subdomen.domen:
     """
+
     def __init__(self, email):
         if not re.fullmatch(r".+@.+\..+", email):
             raise EmailFormatError
         super().__init__(email)
+
 
 class Address(Field):
     """
@@ -83,16 +106,19 @@ class Address(Field):
     Імʼя має задоволняти наступним вимогам:
             1. Довжина повинна бути 2+ символів.
     """
+
     def __init__(self, name):
         if not re.fullmatch(r"^.{3,}", name):
             raise NameFormatError
         super().__init__(name)
+
 
 class Record:
     """
     Клас для зберігання інформації про контакт,
     включаючи ім'я, список телефонів, список Emails, адресу, день народження
     """
+
     def __init__(self, name):
         self.name = Name(name)
         self.phones = []
@@ -117,7 +143,8 @@ class Record:
     def edit_phone(self, old_phone, new_phone):
         # Якщо старий телефон існує, і якщо ми успішно додали новий, та якщо старий та новий не співпадають
         # телефон, то тоді видаляємо старий
-        if self.find_phone(old_phone) and self.add_phone(new_phone) and (self.find_phone(old_phone) != self.add_phone(new_phone)):
+        if self.find_phone(old_phone) and self.add_phone(new_phone) and (
+                self.find_phone(old_phone) != self.add_phone(new_phone)):
             self.remove_phone(old_phone)
             return True
         return False
@@ -146,6 +173,7 @@ class Record:
             self.birthday = Birthday(day=int(day), month=int(month), year=int(year))
         except ValueError as exception:
             raise BirthdayFormatError from exception
+
     def __str__(self):
         return (f"Contact name: {self.name.value}, \n"
                 f"phones: {'; '.join(p.value for p in self.phones)}, \n"
@@ -158,6 +186,7 @@ class AddressBook(UserDict):
     """
     Клас для зберігання адресної книги
     """
+
     def find(self, name):
         return self.data.get(name, None)
 
@@ -215,6 +244,70 @@ class AddressBook(UserDict):
     def __str__(self):
         return 'Address book:\n\t' + '\n\t'.join(record.__str__() for record in self.data.values())
 
-class NoteBook():
-    pass
 
+class Note:
+
+    def __init__(self, text: str):
+        self.raw_text = text
+        self.title = ""
+        self.body = ""
+        self.tags = []  # todo заповнити пізніше
+        self.__extract_title_and_body()
+
+    def __extract_title_and_body(self):
+        """
+        Заголовок може бути лише на початку нотатки і за умови, що він виділений спеціальними символами
+        """
+        start_symbols = '<<'
+        end_symbols = '>>'
+        start_index = self.raw_text.find(start_symbols)
+        end_index = self.raw_text.find(end_symbols)
+
+        if start_index == 0 and end_index != -1:
+            self.title = self.raw_text[len(start_symbols):end_index]
+            self.body = self.raw_text[end_index + len(end_symbols):]
+        else:
+            self.title = ""
+            self.body = self.raw_text
+
+    def __str__(self):
+        if self.title:
+            return f'Title: <<{self.title}>>\nBody: {self.body}'
+        else:
+            return f'Body: {self.body}'
+
+    def short_str(self):
+        if self.title:
+            return self.title
+        elif len(self.body) < 10:
+            return self.body
+        else:
+            return f"{self.body[:10]}..."
+
+
+class NoteBook(UserDict):
+    id = 0
+
+    def load(self):
+        # TODO
+        pass
+
+    def add_note(self, note: Note):
+        self.data[NoteBook.id] = note
+        NoteBook.id += 1
+
+    def delete(self, id_to_delete: int):
+        if self.data.pop(id_to_delete, None):
+            return True
+        return False
+
+    def find_notes_by_text(self, text):
+        result = {}
+        for note_id, note in self.data.items():
+            if text in note.raw_text:
+                result[note_id] = note
+
+        return result
+
+    def find_note_by_id(self, id_to_find):
+        return self.data.get(id_to_find, None)
--- func.py
+++ func.py
@@ -1,11 +1,13 @@
+from classes import NameFormatError, PhoneFormatError, BirthdayFormatError, EmailFormatError, AddressFormatError, \
+    NoTextError, NoIdEnteredError, NoIdFoundError
+from classes import Name, Phone, Address, Birthday, Record, AddressBook, NoteBook, Note
 
-from classes import NameFormatError, PhoneFormatError, BirthdayFormatError, EmailFormatError, AddressFormatError
-from classes import Name, Phone, Address, Birthday, Record, AddressBook
 
 def input_error(func):
     '''
     Судячи з усього - це декоратор обробки помилок
     '''
+
     def inner(*args, **kwargs):
         try:
             return func(*args, **kwargs)
@@ -25,7 +27,15 @@ def input_error(func):
             return "Wrong email format."
         except AddressFormatError:
             return "Wrong email format."
+        except NoTextError:
+            return "Give me text please"
+        except NoIdEnteredError:
+            return "Give me id please"
+        except NoIdFoundError:
+            return "Cannot find id"
+
     return inner
+
 
 def parse_input(user_input: str):
     '''
@@ -36,17 +46,20 @@ def parse_input(user_input: str):
     args_tuple = (*args,)
     return cmd_string, args_tuple
 
+
 def help():
     '''
     Друк сторінки з синтаксисом команд
     '''
-    print("Usage:")    # FIXME наповнити змістом #######################
+    print("Usage:")  # FIXME наповнити змістом #######################
+
 
 def hello():
     '''
     Друк сторінки з привітанням
     '''
-    print("How can I help you?") # FIXME наповнити змістом #######################
+    print("How can I help you?")  # FIXME наповнити змістом #######################
+
 
 @input_error
 def add_man(args: tuple):
@@ -66,40 +79,46 @@ def add_man(args: tuple):
     else:
         print(f"Contact {name} is exist already. Nothing is added.")
 
+
 @input_error
 def del_man(args):
     '''
     Функція видалення контакту
     '''
-    pass # FIXME наповнити кодом #######################
+    pass  # FIXME наповнити кодом #######################
+
 
 @input_error
 def cng_man(args):
     '''
     Функція зміни/оновлення контакту
     '''
-    pass # FIXME наповнити кодом #######################
+    pass  # FIXME наповнити кодом #######################
+
 
 @input_error
 def show_man(args):
     '''
     Функція показу даних контакту
     '''
-    pass # FIXME наповнити кодом #######################
+    pass  # FIXME наповнити кодом #######################
+
 
 @input_error
 def find_man(args):
     '''
     Функція пошуку контакту
     '''
-    pass # FIXME наповнити кодом #######################
+    pass  # FIXME наповнити кодом #######################
+
 
 @input_error
 def add_phone(args):
     '''
     Функція додавання номеру(-ів) телефону(-ів)
     '''
-    pass # FIXME наповнити кодом #######################
+    pass  # FIXME наповнити кодом #######################
+
 
 @input_error
 def cng_phone(args):
@@ -108,12 +127,14 @@ def cng_phone(args):
     '''
     pass  # FIXME наповнити кодом #######################
 
+
 @input_error
 def del_phone(args):
     '''
     Функція зміни номеру телефону
     '''
     pass  # FIXME наповнити кодом #######################
+
 
 @input_error
 def add_email(args):
@@ -122,12 +143,14 @@ def add_email(args):
     '''
     pass  # FIXME наповнити кодом #######################
 
+
 @input_error
 def cng_email(args):
     '''
     Функція додавання email
     '''
     pass  # FIXME наповнити кодом #######################
+
 
 @input_error
 def find_email(args):
@@ -136,12 +159,14 @@ def find_email(args):
     '''
     pass  # FIXME наповнити кодом #######################
 
+
 @input_error
 def del_email(args):
     '''
     Функція видаленя email
     '''
     pass  # FIXME наповнити кодом #######################
+
 
 @input_error
 def add_bday(args):
@@ -150,12 +175,14 @@ def add_bday(args):
     '''
     pass  # FIXME наповнити кодом #######################
 
+
 @input_error
 def cng_bday(args):
     '''
     Функція додавання дня народження
     '''
     pass  # FIXME наповнити кодом #######################
+
 
 @input_error
 def del_bday(args):
@@ -164,12 +191,14 @@ def del_bday(args):
     '''
     pass  # FIXME наповнити кодом #######################
 
+
 @input_error
 def show_bday(args):
     '''
     Функція видалення дня народження
     '''
     pass  # FIXME наповнити кодом #######################
+
 
 @input_error
 def add_adr(args):
@@ -178,12 +207,14 @@ def add_adr(args):
     '''
     pass  # FIXME наповнити кодом #######################
 
+
 @input_error
 def del_adr(args):
     '''
     Функція видалення адреси
     '''
     pass  # FIXME наповнити кодом #######################
+
 
 @input_error
 def find_adr(args):
@@ -194,30 +225,59 @@ def find_adr(args):
 
 
 @input_error
-def add_note(args):
+def add_note(args, notes: NoteBook):
     '''
     Функція додавання нотатки
     '''
-    pass  # FIXME наповнити кодом #######################
+    if len(args) == 0:
+        raise NoTextError()
+
+    text = args[0]
+    note = Note(text)
+    notes.add_note(note)
+    print("Note added")
+
 
 @input_error
-def del_note(args):
+def del_note(args, notes: NoteBook):
     '''
     Функція додавання нотатки
     '''
-    pass  # FIXME наповнити кодом #######################
+    if len(args) == 0:
+        raise NoIdEnteredError()
+    id_to_delete = int(args[0])
+    if notes.delete(id_to_delete):
+        print("Note deleted")
+    else:
+        raise NoIdFoundError()
+
 
 @input_error
-def find_note(args):
+def find_note(args, notes: NoteBook):
     '''
     Функція додавання нотатки
     '''
-    pass  # TODO наповнити кодом #######################
+    if len(args) == 0:
+        raise NoTextError()
+    text = args[0]
+    found_notes = notes.find_notes_by_text(text)
+    if len(found_notes) == 0:
+        print("No notes found")
+    else:
+        print("\n----\n".join(
+            map(lambda note_item: f"{note_item[0]}: {note_item[1].short_str()}", found_notes.items())))
+
 
 @input_error
-def show_note(args):
+def show_note(args, notes: NoteBook):
     '''
     Функція додавання нотатки
     '''
-    pass  # TODO наповнити кодом #######################
-
+    if len(args) == 0:
+        raise NoIdEnteredError()
+    id_to_find = int(args[0])
+    note = notes.find_note_by_id(id_to_find)
+    if note:
+        print(note)
+    else:
+        raise NoIdFoundError()
