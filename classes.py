205ac8800ae58e25a2893fd7e050b5ec3359b506
bohdanino <pokotilobogdan1998@gmail.com>
2024-03-16 12:38:35 +0100

    Deleted __str__ for Record, because there is already one

--- classes.py
+++ classes.py
@@ -167,9 +167,6 @@ class Record:
         self.address = None
         self.birthday = None
     
-    def __str__(self):
-        return f"{self.name} Record"
-    
     def __repr__(self):
         return f"{self.name} Record"
 
