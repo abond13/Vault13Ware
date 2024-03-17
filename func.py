0e3a6a8e0071627a19c0d2bebc0fa99180a9d114
bohdanino <pokotilobogdan1998@gmail.com>
2024-03-17 11:49:55 +0100

    Now find-email prints out contacts and emails containing a given substring

--- func.py
+++ func.py
@@ -342,19 +342,27 @@ def cng_email(args: tuple, book: AddressBook):
 @input_error
 def find_email(args: tuple, book: AddressBook):
     """
-    Функція пошуку пошти.
+    Функція пошуку пошти за підрядком.
+    Виводить список електроних пошт, які мають в собі заданий підрядок
     """
     if len(args) < 1:
         raise MinArgsQuantityError
     
-    name = args[0]
-    found = book.find_email(name)
-    if found:
-        print(f"Contact {name} has email:")
-        for record in found:
-            print(record)
-    else:
-        print("Email not found.")
+    substring = args[0]
+    print(f"\nContacts with '{substring}' in email:\n")
+    flag = 0
+
+    for name, record in book.items():
+        
+        # Перевіряємо, чи є у даного контакта хоча б одна пошта, яка містить в собі підрядок substring
+        if len(list(filter(lambda email: substring in email.value, record.emails))):
+            flag = 1
+            print(f"{name}:")
+            for email in filter(lambda email: substring in email.value, record.emails):
+                print(email)
+            print()
+    if flag == 0:
+        print("Not found.\n")
 
 
 @input_error
